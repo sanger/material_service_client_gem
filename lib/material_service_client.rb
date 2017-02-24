dfdc8009a022c4f2473a4719bc797421036bdb68
Chris Smith <cs24@sanger.ac.uk>
2017-02-24 12:18:08 +0000

    Allow site to be set through module method

--- lib/material_service_client.rb
+++ lib/material_service_client.rb
@@ -3,80 +3,84 @@ require 'faraday'
 require 'json'
 
 module MaterialServiceClient
-	module Material
+
+	class MissingSite < StandardError; end
+
+	def self.site=(site)
+		@site = site
+	end
+
+	def self.site
+		@site || ENV['MATERIALS_URL']
+	end
+
+	class Base
+		def self.connection
+			url = MaterialServiceClient.site
+
+			raise MissingSite.new, 'site must be set on MaterialServiceClient before any requests can be made' if url.nil?
+
+			Faraday.new(:url => url, :headers => {'Content-Type' => 'application/json'}) do |faraday|
+			  faraday.use ZipkinTracer::FaradayHandler, 'eve'
+			  faraday.proxy url
+			  faraday.request  :url_encoded
+			  faraday.response :logger
+			  faraday.adapter  Faraday.default_adapter
+			end
+		end
+	end
+
+	class Material < Base
 		def self.post(data)
-			conn = MaterialServiceClient::get_connection
-			JSON.parse(conn.post('/materials', data.to_json).body)
+			JSON.parse(connection.post('/materials', data.to_json).body)
 		end
 
 		def self.put(data)
 			uuid = data[:uuid]
 			data_to_send = data.reject{|k,v| k.to_sym == :uuid}
 
-			conn = MaterialServiceClient::get_connection
-			JSON.parse(conn.put('/materials/'+uuid, data_to_send.to_json).body)
+			JSON.parse(connection.put('/materials/'+uuid, data_to_send.to_json).body)
 		end
 
 		def self.get(uuid)
 			return nil if uuid.nil?
-			conn = MaterialServiceClient::get_connection
-			JSON.parse(conn.get('/materials/'+uuid).body)
+			JSON.parse(connection.get('/materials/'+uuid).body)
 		end
 
 		def self.valid?(uuids)
-			conn = MaterialServiceClient::get_connection
 			data = { materials: uuids }
 
-			response = conn.post('/materials/validate', data.to_json)
+			response = connection.post('/materials/validate', data.to_json)
 			response.body == 'ok'
 		end
 
 		def self.delete(uuid)
 			return nil if uuid.nil?
-			conn = MaterialServiceClient::get_connection
-			conn.delete('/materials/'+uuid)
+			connection.delete('/materials/'+uuid)
 		end
 	end
 
-	module Container
+	class Container < Base
 		def self.post(data)
-			conn = MaterialServiceClient::get_connection
-			JSON.parse(conn.post('/containers', data.to_json).body)
+			JSON.parse(connection.post('/containers', data.to_json).body)
 		end
 
 		def self.put(data)
 			uuid = data[:uuid]
 			data_to_send = data.reject{|k,v| k.to_sym == :uuid}
 
-			conn = MaterialServiceClient::get_connection
-			JSON.parse(conn.put('/containers/'+uuid, data_to_send.to_json).body)
+			JSON.parse(connection.put('/containers/'+uuid, data_to_send.to_json).body)
 		end
 
 		def self.get(uuid)
 			return nil if uuid.nil?
-			conn = MaterialServiceClient::get_connection
-			JSON.parse(conn.get('/containers/'+uuid).body)
+			JSON.parse(connection.get('/containers/'+uuid).body)
 		end
 
 		def self.delete(uuid)
 			return nil if uuid.nil?
-			conn = MaterialServiceClient::get_connection
-			conn.delete('/containers/'+uuid)
+			connection.delete('/containers/'+uuid)
 		end
-	end
-
-	private
-
-	def self.get_connection
-		conn = Faraday.new(:url => ENV['MATERIALS_URL']) do |faraday|
-		  faraday.use ZipkinTracer::FaradayHandler, 'eve'
-		  faraday.proxy ENV['MATERIALS_URL']
-		  faraday.request  :url_encoded
-		  faraday.response :logger
-		  faraday.adapter  Faraday.default_adapter
-		end
-		conn.headers = {'Content-Type' => 'application/json'}
-		conn
 	end
 
 end
